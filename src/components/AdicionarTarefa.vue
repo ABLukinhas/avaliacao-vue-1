0c6581556448acf709813133f693aaf2774bff1d
ABLukinhas <lucasantoniobrandao008@gmail.com>
2024-08-30 15:22:12 -0300

    recuperaçao dev

---
+++ src/components/AdicionarTarefa.vue
@@ -0,0 +1,28 @@
+<script setup>
+const model = defineModel()
+import ListarTarefas from './ListarTarefas.vue';
+function adicionarTarefa(){
+ adicionarTarefa.push(ListarTarefas) // gambiarra que deu errado
+}
+</script>
+
+<template>
+    <section>
+        <h2>Adicione uma nova tarefa</h2>
+        <input type="text" v-model="model" @keyup.enter="$emit('adicionarTarefa')" />
+        <button @click="adicionarTarefa" >Adicionar</button>
+    </section>
+</template>
+
+<style scoped>
+input {
+    width: 50%;
+    padding: 5px;
+    margin: 5px 0;
+}
+
+button {
+    padding: 5px;
+    margin: 5px 0;
+}
+</style>
---
+++ src/components/ListarTarefas.vue
@@ -0,0 +1,40 @@
+<script setup>
+import AdicionarTarefa from './AdicionarTarefa.vue';
+const tarefas = [];
+
+
+defineProps({
+    realizadas: {
+        type: Boolean,
+        default: false
+    }
+})
+
+// foi mal sor essa nao consigui fazer nada sla que deu comigo deve ser so um dia ruin
+</script>
+
+<template>
+    <section v-for="tarefa in tarefas" :key="tarefa.id" :class="{ 'feita': tarefa.feita }"
+        @click="$emit('click', tarefa.id)">
+        <Transition name="fade">
+            <p v-if="realizadas ? tarefa.feita : !tarefa.feita">{{ tarefa.descricao }}</p>
+        </Transition>
+    </section>
+ 
+</template>
+
+<style scoped>
+.feita {
+    text-decoration: line-through;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+    transition: opacity 2s ease-in-out;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+    opacity: 0;
+}
+</style>
